499efed659addb3d70fab721ac74b7f4bdea1b72
kamran.heydarli <kamran_heyderli@mail.ru>
2025-09-24 16:48:27 +0400

    new

---
+++ src/test/java/GetRequest02.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class GetRequest02 {
+
+    @Test
+    public void get02(){
+
+        //1- lazimli bodyni ve endpointi hazirla
+        String url = "https://restful-booker.herokuapp.com/booking/458";
+        //2- expected bodyni yarat
+        //3- request gonder ve qayidan responseni yadda saxla
+
+
+    }
+}
